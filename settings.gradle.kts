0b92781a578bb2dcdaaddf54d7fcba681256c6e7
Kyle Escobar <33061104+kyle-escobar@users.noreply.github.com>
2023-10-06 10:01:20 -0400

    Disable test-app

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,8 +17,8 @@ include(":asm")
 include(":injector")
 include(":annotations")
 
-include(":test-app")
+/*include(":test-app")
 include(":test-app:app")
 include(":test-app:api")
 include(":test-app:target")
-include(":test-app:mixins")
+include(":test-app:mixins")*/
